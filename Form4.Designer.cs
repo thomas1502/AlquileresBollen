f37f1476e17d331fc69706c0a7bee983d0cf8868
Thomas BC <t.lambert.bc@gmail.com>
2021-03-08 00:45:17 -0600

    Proyecto finalizado.

---
+++ Form4.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace AlquileresBollen
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Clientes:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 48);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1012, 213);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 285);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Vehículos:";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(16, 318);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 62;
+            this.dataGridView2.RowTemplate.Height = 28;
+            this.dataGridView2.Size = new System.Drawing.Size(1012, 207);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 546);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 30);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Alquileres:";
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(16, 580);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 62;
+            this.dataGridView3.RowTemplate.Height = 28;
+            this.dataGridView3.Size = new System.Drawing.Size(1012, 221);
+            this.dataGridView3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 838);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(305, 30);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Alquiler con más recorridos:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(246, 838);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 30);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "0";
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(848, 831);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(180, 37);
+            this.btnActualizar.TabIndex = 8;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1057, 884);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form4";
+            this.Text = "Form4";
+            this.Load += new System.EventHandler(this.Form4_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnActualizar;
+    }
+}
---
+++ Form4.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace AlquileresBollen
+{
+    public partial class Form4 : Form
+    {
+        // Instanciar listas temporales
+        List<Vehiculo> vehiculos = new List<Vehiculo>();
+        List<Cliente> clientes = new List<Cliente>();
+        List<Alquiler> alquileres = new List<Alquiler>();
+        // Funciones propias
+        private void LeerClientes()
+        {
+            FileStream stream = new FileStream("Clientes.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+
+            while (reader.Peek() > -1)
+            {
+                Cliente clienteTemp = new Cliente();
+
+                clienteTemp.Nit = reader.ReadLine();
+                clienteTemp.Nombre = reader.ReadLine();
+                clienteTemp.Direccion = reader.ReadLine();
+
+                clientes.Add(clienteTemp);
+            }
+            reader.Close();
+        }
+        private void LeerVehiculos()
+        {
+            FileStream stream = new FileStream("Vehiculos.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+
+            while (reader.Peek() > -1)
+            {
+                Vehiculo vehiculoTemp = new Vehiculo();
+                vehiculoTemp.Placa = reader.ReadLine();
+                vehiculoTemp.Marca = reader.ReadLine();
+                vehiculoTemp.Modelo = Convert.ToInt32(reader.ReadLine());
+                vehiculoTemp.Color = reader.ReadLine();
+                vehiculoTemp.PrecioKilometros = float.Parse(reader.ReadLine());
+
+                vehiculos.Add(vehiculoTemp);
+            }
+            reader.Close();
+        }
+        private void LeerAlquileres()
+        {
+            FileStream stream = new FileStream("Alquileres.txt", FileMode.OpenOrCreate, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+
+            while (reader.Peek() > -1)
+            {
+                Alquiler alquilerTemp = new Alquiler();
+                alquilerTemp.Nit = reader.ReadLine();
+                alquilerTemp.Nombre = reader.ReadLine();
+                alquilerTemp.Direccion = reader.ReadLine();
+                alquilerTemp.Placa = reader.ReadLine();
+                alquilerTemp.Marca = reader.ReadLine();
+                alquilerTemp.Modelo = Convert.ToInt32(reader.ReadLine());
+                alquilerTemp.Color = reader.ReadLine();
+                alquilerTemp.PrecioKilometro = float.Parse(reader.ReadLine());
+                alquilerTemp.FechaAlquiler = Convert.ToDateTime(reader.ReadLine());
+                alquilerTemp.FechaDevolucion = Convert.ToDateTime(reader.ReadLine());
+                alquilerTemp.KilometrosRecorridos = float.Parse(reader.ReadLine());
+                alquilerTemp.TotalPagar = float.Parse(reader.ReadLine());
+
+                alquileres.Add(alquilerTemp);
+            }
+            reader.Close();
+        }
+        // Funcion para mostrar
+        private void Cargar()
+        {
+            LeerClientes();
+            LeerVehiculos();
+            LeerAlquileres();
+            // Mostramos los datos 
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = clientes;
+            dataGridView1.Refresh();
+
+            dataGridView2.DataSource = null;
+            dataGridView2.DataSource = vehiculos;
+            dataGridView2.Refresh();
+
+            dataGridView3.DataSource = null;
+            dataGridView3.DataSource = alquileres;
+            dataGridView3.Refresh();
+            // Mostramos el alquiler con más km
+            // Ordenamos ascendetemente la lista
+            alquileres = alquileres.OrderByDescending(p => p.KilometrosRecorridos).ToList();
+            // Extraemos el valor más alto
+            label3.Text = (alquileres[0].KilometrosRecorridos).ToString();
+        }
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            Cargar();
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            Cargar();
+            MessageBox.Show("Datos actualizador correctamente.");
+        }
+    }
+}
